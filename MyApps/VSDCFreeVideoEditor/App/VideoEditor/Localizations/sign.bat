c23a01101518cb7f2526216687068f412a1fb9e9
SebCanet <sebastien.canet@ac-nantes.fr>
2016-02-02 21:51:11 +0100

    added new soft
    oCam
    Strut presentation
    VSDCFreeVideoEditor

---
+++ MyApps/VSDCFreeVideoEditor/App/VideoEditor/Localizations/sign.bat
@@ -0,0 +1,4 @@
+call W:\Tools\Sign\sign.bat "Translation" "Russian.dll"
+call W:\Tools\Sign\sign.bat "Translation" "German.dll"
+call W:\Tools\Sign\sign.bat "Translation" "French.dll"
+rem call W:\Tools\Sign\sign.bat "Translation" "Japanese.dll"
